29c6e712c833dda661c6b9857b414bf302c0522e
Lokesh J <lokeshrj@yahoo.com>
2014-07-04 01:12:46 -0700

    First commit

---
+++ Yelp/BusinessTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BusinessTableViewCell.h
+//  Yelp
+//
+//  Created by Lokesh Jagasia on 7/4/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BusinessTableViewCell : UITableViewCell
+
+@end
